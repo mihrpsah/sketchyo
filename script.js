6fdd8bee80b10aca254e2871d64d8bd9726b80da
Mihir Pratap Sah <smihirp1@gmail.com>
2022-07-14 12:49:49 +0530

    v1.0 made

---
+++ script.js
@@ -0,0 +1,16 @@
+for(let i=0; i<256; i++){
+    console.log("JS Triggered")
+    let element = document.createElement("div");
+    element.setAttribute("class", "pixel")
+    let box = document.getElementById("box");
+    box.appendChild(element);
+}
+
+const pixels = document.querySelectorAll('.pixel')
+
+pixels.forEach(pixel => {
+    pixel.addEventListener("mouseover", function(){
+        console.log("Event Selector Working")
+        pixel.setAttribute('style', 'background-color: yellow;');
+    })
+})
