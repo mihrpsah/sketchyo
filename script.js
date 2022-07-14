2b8432aeae0285015753b5a6a52531a890bd20bf
Mihir Pratap Sah <smihirp1@gmail.com>
2022-07-14 16:37:24 +0530

    Dynamic size added

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-for(let i=0; i<256; i++){
-    console.log("JS Triggered")                 
-    let element = document.createElement("div");
-    element.setAttribute("class", "pixel")
-    let box = document.getElementById("box");
-    box.appendChild(element);
-}
 
-const pixels = document.querySelectorAll('.pixel')
+/* Things to be done:-
+   >A prompt to take size.
+   >Make a function for creating box, give grid size as argument.
+*/
 
-pixels.forEach(pixel => {
-    pixel.addEventListener("mouseover", function(){
-        console.log("Event Selector Working")
-        pixel.setAttribute('style', 'background-color: yellow;');
+let gridSize = parseInt(prompt("Enter desired grid size (2-100)", "16"));
+
+generateGrid(gridSize);
+
+function generateGrid(size){
+
+    for(let i=0; i<size*size; i++){
+        console.log("JS Triggered")                 
+        let element = document.createElement("div");
+        element.setAttribute("class", "pixel")
+        let box = document.getElementById("box");
+        box.appendChild(element);
+    }
+    
+    const pixels = document.querySelectorAll('.pixel')
+    
+    pixels.forEach(pixel => {
+        pixel.addEventListener("mouseover", function(){
+            console.log("Event Selector Working")
+            pixel.setAttribute('style', 'background-color: yellow;');
+        })
     })
-})
+    return;
+}
