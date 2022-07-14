f10ae1aee59f46ab10ef29db86f36ed2fef79401
Mihir Pratap Sah <smihirp1@gmail.com>
2022-07-14 17:09:36 +0530

    v2.0 complete

--- script.js
+++ script.js
@@ -8,28 +8,32 @@ let gridSize = parseInt(prompt("Enter desired grid size (2-100)", "16"));
 
 generateGrid(gridSize);
 
-function generateGrid(size){
+function generateGrid(size) {
 
-    for(let i=0; i<size*size; i++){
-        console.log("JS Triggered")                 
+    for (let i = 0; i < size * size; i++) {
+        console.log("JS Triggered")
         let element = document.createElement("div");
         element.setAttribute("class", "pixel")
         let box = document.getElementById("box");
         box.appendChild(element);
     }
-    
+
     const pixels = document.querySelectorAll('.pixel')
     console.log("const pixels defined")
-    
+
+    let sideLength = 800 / size;
+
+
+
+
     pixels.forEach(pixel => {
-        pixel.addEventListener("mouseover", function(){
+        pixel.style.width = sideLength + "px";
+        pixel.style.height = sideLength + "px";
+        pixel.addEventListener("mouseover", function () {
             console.log("Event Selector Working")
-           // pixel.setAttribute('style', 'background-color: yellow;');
             pixel.style.backgroundColor = "yellow";
-            let sideLength = 800/size;
-            pixel.style.width = sideLength + "px";
-            pixel.style.height = sideLength + "px";
         })
     })
+
     return;
 }
